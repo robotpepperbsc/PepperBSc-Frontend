93f65ee1a599c9215a53d53b55234dabda7a9d4a
WookashWackomy <wookie.xp.07@gmail.com>
2019-11-08 15:54:37 +0100

    include scenario list into scenario compoennt

--- pepper-workshop-frontend/src/components/Scenarios/ScenarioList/ScenarioList.js
+++ pepper-workshop-frontend/src/components/Scenarios/ScenarioList/ScenarioList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ScenarioListItem from "./ScenarioListItem";
 
 function ScenarioList(props) {
   const { scenarios } = props;
   return (
     <div>
       {scenarios.map(scenario => {
-        return <div>xd</div>;
+        return (
+          <ScenarioListItem
+            name={scenario.name}
+            description={scenario.description}
+            date={scenario.date}
+          ></ScenarioListItem>
+        );
       })}
     </div>
   );
